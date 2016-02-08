3a8a81f36d4013dd78864a26767a38447ca5a167
Maxim Zhurilo <fumus.27@gmail.com>
2016-02-08 10:14:43 +0200

    more security, voters, locking users

---
+++ src/AppBundle/Security/CommentVoter.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: fumus
+ * Date: 07.02.16
+ * Time: 16:04
+ */
+
+namespace AppBundle\Security;
+
+
+class CommentVoter
+{
+
+}
---
+++ src/AppBundle/Security/PostVoter.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: fumus
+ * Date: 07.02.16
+ * Time: 18:44
+ */
+
+namespace AppBundle\Security;
+
+
+class PostVoter
+{
+
+}
